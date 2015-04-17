28f176806c56327b52bed310f379242871942bc6
artsoroka <githubrepository@yandex.ru>
2015-04-17 18:41:53 -0400

    updated parser.js to use new Parser functions

--- parser.js
+++ parser.js
@@ -3,11 +3,26 @@ var images = [];
 var config = {
 	url: 'http://www.bikeexif.com/category/custom-motorcycles/page/',
 	start: 1, 
-	limit: 3,
-	saveToFile: 'custom-motorcycles.txt'
+	limit: 2,
+	saveToFile: 'custom-motorcycles.txt', 
+	dir: 'downloads'
 }; 
 
 parser.findPostUrls(config, function(err, pages){
 	if( err ) return console.log(err); 
-	console.log(pages); 
+
+	parser.findImages(pages, function(err, images){
+		if(err) return console.log(err); 
+		
+		console.log('total images found: ' + images.length);
+
+		parser.download({
+			images: images, 
+			dir: config.dir 
+		}, function(err){
+			console.log(err); 
+		});  
+
+	});
+
 }); 
