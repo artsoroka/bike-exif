608f0fc1c883723b88e2c600f665266b71b01924
artsoroka <githubrepository@yandex.ru>
2015-04-18 06:54:56 -0400

    promises based parser

---
+++ parse.js
@@ -0,0 +1,21 @@
+var Promise = require('bluebird');  
+var parser  = require('./parser/');
+
+parser
+	.findPostUrls({
+		url: 'http://www.bikeexif.com/category/custom-motorcycles/page/',
+		start: 1, 
+		limit: 2,
+		saveToFile: 'custom-motorcycles.txt', 
+		dir: 'downloads'
+	})
+	.then(parser.findImages)
+	.then(parser.download({
+		saveFilesTo: 'downloads'
+	}))
+	.then(function(numberOfImages){
+		console.log('images saved: ', numberOfImages); 
+	})
+	.catch(function(err){
+		console.log('ERROR: ', err); 
+	}); 
